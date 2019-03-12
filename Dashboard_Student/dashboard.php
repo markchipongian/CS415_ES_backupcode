1050e92ca7c657ce2df4655ce5d1345983967213
Patrick Sharma <42792639+patsharma@users.noreply.github.com>
2019-03-12 12:13:10 +1200

    Updated profile pic
    Updated profile

--- Dashboard_Student/dashboard.php
+++ Dashboard_Student/dashboard.php
@@ -45,6 +45,37 @@ foreach($details as $row){
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
 
     <script>
+        var slideIndex = 3;
+        showSlides(slideIndex);
+        n = 3;
+        function plusSlides(n) {
+            showSlides(slideIndex += n);
+        }
+
+        function currentSlide(n) {
+            showSlides(slideIndex = n);
+        }
+
+        function showSlides(n) {
+            var i;
+            var slides = document.getElementsByClassName("mySlides");
+            var dots = document.getElementsByClassName("dot");
+            if (n > slides.length) { slideIndex = 1 }
+            if (n < 1) { slideIndex = slides.length }
+            for (i = 0; i < slides.length; i++) {
+                slides[i].style.display = "none";
+            }
+            for (i = 0; i < dots.length; i++) {
+                dots[i].className = dots[i].className.replace(" active", "");
+            }
+            slides[slideIndex - 1].style.display = "block";
+            dots[slideIndex - 1].className += " active";
+        } 
+    </script>
+
+
+
+    <!-- <script>
     // $("#slideshow > div:gt(3)").hide();
 
     //     setInterval(function() {
@@ -85,7 +116,7 @@ foreach($details as $row){
     // Run function when page loads
     window.onload=changeImg;
 
-    </script>
+    </script> -->
 </head>
 
 <body>
@@ -133,8 +164,28 @@ foreach($details as $row){
             <p>This is your homepage and the root of the entire website, you can navigate to other pages from here.</p>
         </div>
         <div class="details">
-            <div id="slideshow">
+            <!-- <div id="slideshow">
                 <img name="slide" style="width:100%;">
+            </div> -->
+           
+            <div id="slideshow">
+                <div class="mySlides" style = "display: block;">
+                    <div  class="numbertext">1 / 3</div>
+                    <img src="../images/1.jpg" style=" width:100%; height: 100%;">
+                </div>
+
+                <div class="mySlides">
+                    <div class="numbertext">2 / 3</div>
+                    <img src="../images/2.jpg" style="width:100%; height:100%; ">
+                </div>
+
+                <div class="mySlides">
+                    <div class="numbertext">3 / 3</div>
+                    <img src="../images/profile_pic.png" style="width:100%; height: 100%;">
+                </div>
+
+                <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
+                <a class="next" onclick="plusSlides(1)">&#10095;</a>
             </div>
 
             <div class="caption">
